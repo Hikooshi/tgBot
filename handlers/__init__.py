b0062e10ca2d1a33c88b1aaf1fb937451578f9ea
Hikooshi <hikooshi@yandex.ru>
2023-04-24 12:56:08 +0300

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+from tgBot.handlers.exchange import dp
+from tgBot.handlers.cute import dp
+from tgBot.handlers.weather import dp
+from tgBot.handlers.polls import dp
+from tgBot.handlers.greeting import dp
